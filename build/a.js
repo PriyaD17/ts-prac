71d6a6e26a40888729de2cb2e17384426fc5ce53
Priya <ganapathipriyadharsani@gmail.com>
2025-01-08 17:30:16 +0530

    enums

--- build/a.js
+++ build/a.js
@@ -1,3 +1,27 @@
 "use strict";
-let a = "hello world";
-console.log(a);
+var Direction;
+(function (Direction) {
+    Direction[Direction["up"] = 0] = "up";
+    Direction[Direction["down"] = 1] = "down";
+    Direction[Direction["left"] = 2] = "left";
+    Direction[Direction["right"] = 3] = "right";
+})(Direction || (Direction = {}));
+var Disha;
+(function (Disha) {
+    Disha["up"] = "upar";
+    Disha["down"] = "neeche";
+    Disha["left"] = "baain";
+    Disha["right"] = "daain";
+})(Disha || (Disha = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2[Direction2["up"] = 17] = "up";
+    Direction2[Direction2["down"] = 21] = "down";
+    Direction2[Direction2["left"] = 22] = "left";
+    Direction2[Direction2["right"] = 23] = "right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction.down);
+console.log(Direction.left);
+console.log(Disha.down);
+console.log(Direction2.down);
+console.log(Direction2.left);
--- src/a.ts
+++ src/a.ts
@@ -1,13 +1,25 @@
-interface User{
-    firstName: String,
-    lastName: String,
-    age: number,
-    dob:any
+enum Direction{
+    up,
+    down,
+    left,
+    right
+}
+enum Disha{
+    up="upar",
+    down="neeche",
+    left="baain",
+    right="daain"
+}
+enum Direction2{
+    up=17,
+    down=21,
+    left,
+    right
 }
 
-function isLegal(user: User){
-    if(user.age>=18){
-        return true;
-    }
-    return false;
-}
+console.log(Direction.down);
+console.log(Direction.left);
+console.log(Disha.down);
+console.log(Direction2.down);
+console.log(Direction2.left);
+
